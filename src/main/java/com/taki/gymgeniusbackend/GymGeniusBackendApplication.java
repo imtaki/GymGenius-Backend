68b3f614fb3a5653a3718a3a749a76863a87d523
imtaki <161586687+imtaki@users.noreply.github.com>
2025-09-22 18:29:31 +0200

    init

---
+++ src/main/java/com/taki/gymgeniusbackend/GymGeniusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.taki.gymgeniusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymGeniusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymGeniusBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taki/gymgeniusbackend/GymGeniusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taki.gymgeniusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymGeniusBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
